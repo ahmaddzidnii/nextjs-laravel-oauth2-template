a7fef5683f49d3e874edccd11bc8d5913d496dc5
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-01-10 15:16:29 +0700

    fix(be): correct formatting in app.php for improved readability and consistency

--- backend-laravel/bootstrap/app.php
+++ backend-laravel/bootstrap/app.php
@@ -9,15 +9,15 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         // web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request$request) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
                     'message' => 'route not found',
