e685fd27af750ab082b64a346d647e63f2956c1a
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-01-17 10:25:28 +0700

    refactor(be): simplify constructor in ProfileController by removing redundant assignment

--- backend-laravel/app/Http/Controllers/Profile/ProfileController.php
+++ backend-laravel/app/Http/Controllers/Profile/ProfileController.php
@@ -6,16 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Services\AuthService;
 use App\Traits\ApiResponseHelper;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ProfileController extends Controller
 {
     use ApiResponseHelper;
 
-    public function __construct(protected readonly AuthService $authService)
-    {
-        $this->authService = $authService;
-    }
+    public function __construct(protected readonly AuthService $authService) {}
 
     public function me(Request $request)
     {
